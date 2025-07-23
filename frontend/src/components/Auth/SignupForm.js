47f244e6e504934750af2c258278f2fb22bfd4fd
Arnabpahari <156195450+Arnabpahari@users.noreply.github.com>
2025-07-23 11:10:23 +0530

    Update SignupForm.js

--- frontend/src/components/Auth/SignupForm.js
+++ frontend/src/components/Auth/SignupForm.js
@@ -12,7 +12,7 @@ const SignupForm = () => {
     const handleSubmit = async e => {
         e.preventDefault();
         try {
-            const res = await axios.post('http://localhost:5000/api/auth/signup', formData);
+            const res = await axios.post('https://moodmate-backend-bzmq.onrender.com/api/auth/signup', formData);
             localStorage.setItem('token', res.data.token);
             localStorage.setItem('username', res.data.username);
             window.location.href = '/home';  // Redirect to HomePage
