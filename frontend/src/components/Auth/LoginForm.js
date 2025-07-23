12ecc0f7b006589ecf2c14968a92acadc0fbd76b
Arnabpahari <156195450+Arnabpahari@users.noreply.github.com>
2025-07-23 14:59:50 +0530

    Update LoginForm.js

--- frontend/src/components/Auth/LoginForm.js
+++ frontend/src/components/Auth/LoginForm.js
@@ -9,7 +9,7 @@ const LoginForm = ({ onLoginSuccess }) => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post('http://localhost:5000/api/auth/login', {
+            const response = await axios.post('https://moodmate-backend-bzmq.onrender.com/api/auth/login', {
                 username,
                 password
             });
