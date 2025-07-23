32388b419e930006171ead443a6a33a0d37f4b79
Arnabpahari <156195450+Arnabpahari@users.noreply.github.com>
2025-07-23 10:35:57 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,24 +5,32 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors());
+// Configure CORS properly
+const allowedOrigins = ['https://moodmate-frontend.onrender.com'];
+
+app.use(cors({
+    origin: allowedOrigins,
+    credentials: true, // Enable if using cookies/session
+}));
+
 app.use(express.json());
+
+// Routes
 const authRoutes = require('./routes/authRoutes.js');
 app.use('/api/auth', authRoutes);
 
+const postRoutes = require('./routes/postRoutes');
+app.use('/api/posts', postRoutes);
 
+// Root check
 app.get('/', (req, res) => res.send('MoodMate API is running'));
 
-
+//  Mongo connection + start server
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
     console.log('MongoDB connected');
     app.listen(5000, () => console.log('Server running on port 5000'));
 })
 .catch(err => console.log(err));
-
-
-const postRoutes = require('./routes/postRoutes');
-app.use('/api/posts', postRoutes);
 
 
