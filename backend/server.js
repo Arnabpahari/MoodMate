8f747695a810ed2e0a383dc96c9a9472f3547099
Arnabpahari <156195450+Arnabpahari@users.noreply.github.com>
2025-08-19 19:52:54 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ app.get('/', (req, res) => res.send('MoodMate API is running'));
 mongoose.connect(process.env.MONGO_URI)
     .then(() => {
         console.log('MongoDB connected');
-        const PORT = process.env.PORT || 5000; // ✅ use platform's assigned port if deployed
+        const PORT = process.env.PORT || 5000; 
         app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
     })
     .catch(err => console.log(err));
